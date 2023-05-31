b1f872f6f6ca1e072cf1df48d97a7200ec8d670a
winterbreaker23 <johnjamesdev@hotmail.com>
2023-05-31 19:37:16 +0100

    list2

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -20,8 +20,30 @@ function Garage() {
     </>
   );
 }
+
+function Car2(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+function Garage2() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
+    <>
+	    <h1>Who lives in my garage?</h1>
+	    <ul>
+        {cars.map((car) => <Car2 key={car.id} brand={car.brand} />)}
+      </ul>
+    </>
+  );
+}
+
+
 root.render(
-  <Garage/>
+  <Garage2/>
 );
 
 // If you want to start measuring performance in your app, pass a function
